63e3644b51a267b16b0d138eaf367724803825fd
r0c0328 <ruhi.chandra@walmart.com>
2022-07-18 21:44:59 +0530

    added repo cards

--- repo-app/src/components/repo-list.tsx
+++ repo-app/src/components/repo-list.tsx
@@ -5,43 +5,29 @@ const RepoList = ({
 }: any) => {
    
     const repoList = repoData.items;
-    useEffect(() => {
-        console.log("inside use--- ", repoList);
-      }, [repoList])
-  
     if(!repoList?.length){
         return <div>No repos found!</div>
     }
 
-
     console.log("repoList-------- ", repoList);
     return (
+        <>
+        <p>{repoList && "aa" + repoList[0].id}</p>
         <ul className="repo-list-container">
-            
-            {/* {repoData.avatar_url ? (<div className="dataitem">
-               <img src={repoData.avatar_url} 
-               alt="avatar" /></div>) : (<div></div>)}
-            {repoData.login ? (<div className="dataitem">Login: 
-            {repoData.login}</div>) : (<div></div>)}
-            {repoData.name ? (<div className="dataitem">
-            Name : {repoData.name}</div>) : (<div></div>)}
-            {repoData.blog ? (<div className="dataitem">
-            Blog: {repoData.blog}</div>) : (<div></div>)} */}
-           
-            {repoList?.map((repo: any) => {
+     
+            {repoList.map((repo: any) => 
                 <li className="repo-list-item">
                 <div className="repo-card">
-                    <div>
-                        <img src={repo?.owner.avatar_url} alt="avatar"/>
+                        <img className="avatar-image" src={repo?.owner.avatar_url} alt="avatar"/>
                         <div className="repo-details">
-                            <span className="repo-name">{repo?.name}</span>
-                            <span className="repo-name">{repo?.description}</span>
+                            <div className="repo-name">Repo: {repo?.name}</div>
+                            <div className="repo-about">About: {repo?.description}</div>
                         </div>
-                    </div>
                 </div>
                 </li>
-            })}
+            )}
         </ul>
+        </>
     );
 }
   

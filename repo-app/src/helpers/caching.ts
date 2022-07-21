446dd83ab9d3a28439dc21e489edf7dc9954bfff
r0c0328 <ruhi.chandra@walmart.com>
2022-07-21 15:54:12 +0530

    bug fix

--- repo-app/src/helpers/caching.ts
+++ repo-app/src/helpers/caching.ts
@@ -1,5 +1,5 @@
 export const getCachedResults = () => {
   const savedQuery = localStorage.getItem("searchQuery");
-  const savedData = localStorage.getItem(`repo-data-${savedQuery}`) as string;
-  return { savedData: JSON.parse(savedData) };
+  let savedData = localStorage.getItem(`repo-data-${savedQuery}`) as string;
+  return { savedData: savedData ? JSON.parse(savedData) : null };
 };

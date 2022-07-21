869bce3cd64cd9c5451b6264000de132f3bcd56c
r0c0328 <ruhi.chandra@walmart.com>
2022-07-21 17:00:26 +0530

    edge case

--- repo-app/src/components/repo-item.tsx
+++ repo-app/src/components/repo-item.tsx
@@ -2,10 +2,16 @@ import { RepoItemProps } from "../typedef/typedef";
 
 // This component gives the repo individual attributes 
 const RepoItem = ({ name, value }: RepoItemProps) => {
+  let itemVal = value;
+  // edge case
+  if (value.indexOf(' ') === -1) {
+    itemVal = value.substring(0, 15);
+  }
+  console.log("value.substring(0, 100)", value)
   return (
     <div className="repo-card-attr">
       <span className="repo-card-attr-name">{name}:</span>
-      <span className="repo-card-attr-value">{value}</span>
+      <span className="repo-card-attr-value">{itemVal}</span>
     </div>
   );
 };
